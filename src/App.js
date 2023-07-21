fb136391f0ddf0f83aac3f7ca97f2f751eba0d81
VuranokYigit <fyvuranokk@gmail.com>
2023-07-21 15:37:23 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from "./components/Navbar/navbar";
+import Main from "./components/Main/main";
+import Footer from "./components/Footer/footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
