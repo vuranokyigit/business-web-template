779bd573627afd35e6b582de0a9756e67f0ecf96
VuranokYigit <fyvuranokk@gmail.com>
2023-07-29 00:34:37 +0300

    footerDesgn

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -9,16 +9,16 @@ const Footer = () => {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-6" >
-                            Contact Us Today
+                            <h2>Contact Us Today</h2>
                             <div className="col-md-12">
-                                Main Office
-                                123 Anywhere St.
-                                Any City ST 12345
+                                <h3>Main Office
+                                    123 Anywhere St.
+                                    Any City ST 12345</h3>
                             </div>
                             <div className="col-md-12">
-                                Tel. (123) 456-7890
-                                Email: hello@reallygreatsite.com
-                                Social: @reallygreatsite
+                                <h3> Tel. (123) 456-7890
+                                    Email: hello@reallygreatsite.com
+                                    Social: @reallygreatsite</h3>
                             </div>
                             <button type="button" className="btn btn-secondary btn-lg">Get in Touch</button>
                             <section name="maps" id="maps" class="wow fadeIn">
@@ -26,14 +26,10 @@ const Footer = () => {
                                 <div className="mapouter">
                                     <div className="gmap_canvas">
                                         <iframe
-                                            width="80%"
-                                            height="200px"
+                                            width="100%"
+                                            height="150px"
                                             id="gmap_canvas"
                                             src="https://maps.google.com/maps?q=mirdag hukuk&t=k&z=15&ie=UTF8&iwloc=&output=embed"
-                                            frameBorder={0}
-                                            scrolling="no"
-                                            marginHeight={0}
-                                            marginWidth={0}
                                         />
 
                                         <br />
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -120,9 +120,6 @@ const Main = () => {
                             <h6 className="col-sm-8">See why we're the best
                                 in Venston Bay</h6>
                         </div>
-                        <div className="col-md-2">
-                            <FontAwesomeIcon icon={faComments} style={{ color: "#000000", }} />
-                        </div>
                         <div className="row" id="">
                             <div className="col-md-4">
                                 <div className="card" >
@@ -147,7 +144,7 @@ const Main = () => {
                                     <div className="card-body">
                                         <h5 className="card-title">Card title</h5>
                                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <button className="btn btn-light">Go somewhere</button>
+                                        <button className="btn btn-secondary">Go somewhere</button>
                                     </div>
                                 </div>
                             </div>
