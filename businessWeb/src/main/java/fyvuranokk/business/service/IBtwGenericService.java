ef2975e0bf9a7f263833397a9a84cb29b5b61d8d
VuranokYigit <fyvuranokk@gmail.com>
2023-08-07 15:04:18 +0300

    genericService

--- businessWeb/src/main/java/fyvuranokk/business/service/IBtwGenericService.java
+++ businessWeb/src/main/java/fyvuranokk/business/service/IBtwGenericService.java
@@ -5,9 +5,36 @@ import fyvuranokk.business.dto.BtwDto;
 import fyvuranokk.data.entity.BaseEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IBtwGenericService<D, E> extends IProfileService{
+    //#####Model Mapper####
+    public D EntityToDto(E e);
+    public E DtoToEntity(D d);
+    //#######CRUD#########
+    //Create
+    public D btwServiceCreate(D d);
+    //List
+    public List<D> btwServiceList();
+    //Find (ben sana bi id veriyim o id ye gore bilgileri bul)
+    public D btwServiceFindById(Long id);
+    //Delete
+    public D btwServiceDeleteById(Long id);
+    // Update
+    public D btwServiceUpdateById(Long id,  BtwDto btwDto);
+
+    @Transactional
+
+    //######Pageable#######
+    //list pageable structure
+    public  List<D> btwServiceAllList();
+    //list page and page size
+    public Page<E> btwServicePagination(int currentPage, int pageSize);
+    //List page and pageable
+    public  Page<E> btwServicePagination(Pageable pageable, D d);
 
 }
--- businessWeb/src/main/java/fyvuranokk/business/service/impl/BtwServiceImpl.java
+++ businessWeb/src/main/java/fyvuranokk/business/service/impl/BtwServiceImpl.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,6 +33,56 @@ public class BtwServiceImpl implements IBtwGenericService<BtwDto, BwtEntity> {
 
     @Override
     public String appInformationService(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+
+    @Override
+    public BtwDto EntityToDto(BwtEntity bwtEntity) {
+        return null;
+    }
+
+    @Override
+    public BwtEntity DtoToEntity(BtwDto btwDto) {
+        return null;
+    }
+
+    @Override
+    public BtwDto btwServiceCreate(BtwDto btwDto) {
+        return null;
+    }
+
+    @Override
+    public List<BtwDto> btwServiceList() {
+        return null;
+    }
+
+    @Override
+    public BtwDto btwServiceFindById(Long id) {
+        return null;
+    }
+
+    @Override
+    public BtwDto btwServiceDeleteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public BtwDto btwServiceUpdateById(Long id, BtwDto btwDto) {
+        return null;
+    }
+
+    @Override
+    public List<BtwDto> btwServiceAllList() {
+        return null;
+    }
+
+    @Override
+    public Page<BwtEntity> btwServicePagination(int currentPage, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public Page<BwtEntity> btwServicePagination(Pageable pageable, BtwDto btwDto) {
         return null;
     }
 }
