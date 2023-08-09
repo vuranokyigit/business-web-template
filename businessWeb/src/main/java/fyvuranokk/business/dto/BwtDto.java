7246714ee2ef9d4e3708ebc6309327960fb764d0
VuranokYigit <fyvuranokk@gmail.com>
2023-08-09 11:39:07 +0300

    bwtDtoAddAnnoti

--- businessWeb/src/main/java/fyvuranokk/business/dto/BwtDto.java
+++ businessWeb/src/main/java/fyvuranokk/business/dto/BwtDto.java
@@ -3,16 +3,25 @@ package fyvuranokk.business.dto;
 import fyvuranokk.audit.AuditingAwareBaseDto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.Serializable;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
 public class BwtDto extends AuditingAwareBaseDto implements Serializable {
     public static final Long serialVersionUID=1L;
-    @NotEmpty(message = "user.name.validation.constraints.NotNull.message")
-    @Size(min = 40, message = "user.name.least.validation.constraints.NotNull.message")
+    @NotEmpty(message = "{user.name.validation.constraints.NotNull.message}")
+    @Size(min = 40, message = "{user.name.least.validation.constraints.NotNull.message}")
     private  String name;
-    @NotEmpty(message = "user.email.validation.constraints.NotNull.message")
-    @Size(min = 40, message = "user.email.least.validation.constraints.NotNull.message")
+    @NotEmpty(message = "{user.email.validation.constraints.NotNull.message}")
+    @Size(min = 40, message = "{user.email.least.validation.constraints.NotNull.message}")
     private String mail;
     @NotEmpty(message = "{bwt.topic.validation.constraints.NotNull.message}")
     @Size(min = 4, message = "{bwt.topic.least.validation.constraints.NotNull.message}")
