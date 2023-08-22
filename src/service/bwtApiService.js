15bdcafb9a94052cb05e7d39c1d43a077c568bba
VuranokYigit <fyvuranokk@gmail.com>
2023-08-22 10:52:51 +0300

    apiServiceUpdated

--- src/service/bwtApiService.js
+++ src/service/bwtApiService.js
@@ -1,35 +1,41 @@
-// axios
-import axios from "axios"
 
-// PERSISTE URL
-const REACT_URL = "/bwt/api/v1"
+import axios from 'axios';
 
-// CLASS
-class BwtApiServices {
-    // CREATE
-    // localhost:3333/todo/api/v1/create
-    bwtServiceCreate(bwtDto) {
-        return axios.post(REACT_URL + "/create", bwtDto);
-    }
-    // LIST
-    // localhost:3333/todo/api/v1/list
-    bwtServiceList() {
-        return axios.get(`${REACT_URL}/list`);
-    }
-    // FIND
-    // localhost:3333/todo/api/v1/find/id(0,1,2)
-    bwtServiceFindById(id) {
-        return axios.get(`${REACT_URL}/find/${id}`);
-    }
-    // DELETE
-    // localhost:3333/todo/api/v1/delete/id(0,1,2)
-    bwtServiceDeleteById(id) {
-        return axios.delete(`${REACT_URL}/delete/${id}`);
-    }
-    // DELETE ALL
-    // localhost:3333/todo/api/v1/all/delete
-    bwtServiceDeleteAll() {
-        return axios.get(`${REACT_URL}/all/delete`);
-    }
+const REACT_URL = 'http://localhost:3333/bwt/api/v1'; // Örnek URL
+
+function BwtApiServices() {
+  // CREATE
+  const bwtServiceCreate = (bwtDto) => {
+    return axios.post(`${REACT_URL}/create`, bwtDto);
+  };
+
+  // LIST
+  const bwtServiceList = () => {
+    return axios.get(`${REACT_URL}/list`);
+  };
+
+  // FIND
+  const bwtServiceFindById = (id) => {
+    return axios.get(`${REACT_URL}/find/${id}`);
+  };
+
+  // DELETE
+  const bwtServiceDeleteById = (id) => {
+    return axios.delete(`${REACT_URL}/delete/${id}`);
+  };
+
+  // DELETE ALL
+  const bwtServiceDeleteAll = () => {
+    return axios.get(`${REACT_URL}/all/delete`);
+  };
+
+  return {
+    bwtServiceCreate,
+    bwtServiceList,
+    bwtServiceFindById,
+    bwtServiceDeleteById,
+    bwtServiceDeleteAll,
+  };
 }
-export default new BwtApiServices();
+
+export default BwtApiServices();
